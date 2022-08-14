b732194f8f8fb6bedfe685259e9cce24869749fb
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-08-14 10:31:07 +0800

    transfer imports to functions

--- bot.py
+++ bot.py
@@ -1,20 +1,10 @@
-# import os
 import time
 import random
 import spintax
 import requests
 import config
-# import numpy as np
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from random import randint, randrange
 from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-# from selenium.common.exceptions import NoSuchElementException
-# from webdriver_manager.chrome import ChromeDriverManager as CM
 from functions import *
 
 
