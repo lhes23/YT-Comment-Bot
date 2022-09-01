6e1aa7a8406d9802285ea57c8e1fe48c1d18929c
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-09-01 21:18:50 +0800

    remove underscore from good job comment

--- functions.py
+++ functions.py
@@ -32,7 +32,7 @@ def youtube_login(email, password):
     # driver = webdriver.Chrome(options=op, executable_path=CM().install())
 
     driver = uc.Chrome(use_subprocess=True,options=op, executable_path=CM().install())
-    driver.execute_script("document.body.style.zoom='80%'")
+    driver.execute_script("document.body.style.zoom='100%'")
 
     driver.get('https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
 
@@ -134,7 +134,7 @@ def random_comment():
         'Loved it',
         'Hello',
         '-loved it-',
-        '_good job_'
+        'good job'
 
     ]
     r = np.random.randint(0, len(comments))
